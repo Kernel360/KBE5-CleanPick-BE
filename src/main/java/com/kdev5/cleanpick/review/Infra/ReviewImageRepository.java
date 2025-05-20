fe1db21f5502e78853e6e1d6705ea839687e7d47
andy <yuniugi1234@gmail.com>
2025-05-20 14:53:58 +0900

    reviewImage 오타 수정

--- src/main/java/com/kdev5/cleanpick/review/Infra/ReviewImageRepository.java
+++ src/main/java/com/kdev5/cleanpick/review/Infra/ReviewImageRepository.java
@@ -1,7 +1,7 @@
 package com.kdev5.cleanpick.review.Infra;
 
-import com.kdev5.cleanpick.review.domain.ReviewImage;
+import com.kdev5.cleanpick.review.domain.ReviewFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
+public interface ReviewImageRepository extends JpaRepository<ReviewFile, Long> {
 }
