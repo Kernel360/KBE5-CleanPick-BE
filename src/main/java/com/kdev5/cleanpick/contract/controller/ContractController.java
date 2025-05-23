08bfdb6bbd08573ea415994e77d78db50216de4c
banxzxx <jwbang1643@gmail.com>
2025-05-23 12:05:02 +0900

    refactor : api 경로 이름 수정

--- src/main/java/com/kdev5/cleanpick/contract/controller/ContractController.java
+++ src/main/java/com/kdev5/cleanpick/contract/controller/ContractController.java
@@ -20,21 +20,16 @@ public class ContractController {
 
     private final ContractService contractService;
 
-    @PostMapping("/test")
-    public ApiResponse<Contract> test(@RequestBody @Valid Contract contract) {
-        return ApiResponse.ok(contract);
-    }
-
     // 1. 청소 요청 글 작성
     // 1-1. 단기 청소
-    @PostMapping("/createOneContract")
+    @PostMapping("/one")
     public ApiResponse<ContractRequestDto> createContract(@RequestBody @Valid ContractRequestDto contractDto) {
         ContractRequestDto newContract = contractService.createOneContract(contractDto);
         return ApiResponse.ok(newContract);
     }
 
     // 1-2. 정기 청소
-    @PostMapping("/createRoutineContract")
+    @PostMapping("/routine")
     public ApiResponse<List<ContractRequestDto>> createRoutineContract(@RequestBody @Valid ContractRequestDto contractRequestDto) {
         RoutineContract newRoutineContract = contractService.createRoutineContract(contractRequestDto);
         List<ContractRequestDto> newContractList = new ArrayList<>();
@@ -42,7 +37,7 @@ public class ContractController {
         // startTime을 기준으로 각 예약 DTO에 ContractDate 부여
         LocalDateTime currDate = contractRequestDto.getStartTime();
         for ( int i = 0 ; i < contractRequestDto.getRoutineCount() ; i++ ){
-            contractRequestDto.setContractDate(currDate);
+//            contractRequestDto.setContractDate(currDate);
             ContractRequestDto singleContract = contractService.createOneContract(contractRequestDto);
             newContractList.add(singleContract);
             currDate = currDate.plusDays(1);
