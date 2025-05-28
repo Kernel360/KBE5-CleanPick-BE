269b7e15430ba0e9cc34475cb5154bee6022568e
mummhy0811 <mummhy0811@gmail.com>
2025-05-28 15:26:13 +0900

    feat: 1:1요청 수락 기능 개발 및 함수통합

--- src/main/java/com/kdev5/cleanpick/contract/controller/MatchingController.java
+++ src/main/java/com/kdev5/cleanpick/contract/controller/MatchingController.java
@@ -1,5 +1,6 @@
 package com.kdev5.cleanpick.contract.controller;
 
+import com.kdev5.cleanpick.contract.domain.enumeration.MatchingStatus;
 import com.kdev5.cleanpick.contract.service.ContractMatchingService;
 import com.kdev5.cleanpick.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,18 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/matching")
 public class MatchingController {
     private final ContractMatchingService contractMatchingService;
-
+    
     @PatchMapping("/{contractId}/reject")
     public ResponseEntity<ApiResponse<Void>> rejectMatchingRequest(@PathVariable("contractId") Long contractId) {
         Long managerId = 1L; //TODO: 추후 연결
-        contractMatchingService.rejectMatchingRequest(managerId, contractId);
+        contractMatchingService.updateMatchingStatus(managerId, contractId, MatchingStatus.REJECT);
         return ResponseEntity.ok(ApiResponse.ok());
     }
 
     @PatchMapping("/{contractId}/accept")
     public ResponseEntity<ApiResponse<Void>> acceptMatchingRequest(@PathVariable("contractId") Long contractId) {
         Long managerId = 1L; //TODO: 추후 연결
-        contractMatchingService.acceptMatchingRequest(managerId, contractId);
+        contractMatchingService.updateMatchingStatus(managerId, contractId, MatchingStatus.ACCEPT);
+        return ResponseEntity.ok(ApiResponse.ok());
+    }
+
+    @PatchMapping("/{contractId}/accept-personal")
+    public ResponseEntity<ApiResponse<Void>> acceptPersonalMatchingRequest(@PathVariable("contractId") Long contractId) {
+        Long managerId = 1L; //TODO: 추후 연결
+        contractMatchingService.matchManagerAndCustomer(managerId, contractId);
         return ResponseEntity.ok(ApiResponse.ok());
     }
 }
--- src/main/java/com/kdev5/cleanpick/contract/service/ContractMatchingService.java
+++ src/main/java/com/kdev5/cleanpick/contract/service/ContractMatchingService.java
@@ -1,10 +1,16 @@
 package com.kdev5.cleanpick.contract.service;
 
+import com.kdev5.cleanpick.contract.domain.Contract;
 import com.kdev5.cleanpick.contract.domain.Nominee;
 import com.kdev5.cleanpick.contract.domain.enumeration.MatchingStatus;
+import com.kdev5.cleanpick.contract.domain.exception.ContractException;
 import com.kdev5.cleanpick.contract.domain.exception.NomineeException;
+import com.kdev5.cleanpick.contract.infra.ContractRepository;
 import com.kdev5.cleanpick.contract.infra.NomineeRepository;
 import com.kdev5.cleanpick.global.exception.ErrorCode;
+import com.kdev5.cleanpick.manager.domain.Manager;
+import com.kdev5.cleanpick.manager.domain.exception.ManagerNotFoundException;
+import com.kdev5.cleanpick.manager.infra.repository.ManagerRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,19 +19,29 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ContractMatchingService {
     private final NomineeRepository nomineeRepository;
+    private final ContractRepository contractRepository;
+    private final ManagerRepository managerRepository;
 
     @Transactional
-    public void rejectMatchingRequest(Long managerId, Long contractId) {
+    public void updateMatchingStatus(Long managerId, Long contractId, MatchingStatus status) {
         Nominee nominee = getNominee(managerId, contractId);
-        nominee.updateStatus(MatchingStatus.REJECT);
+        nominee.updateStatus(status);
         nomineeRepository.save(nominee);
     }
 
     @Transactional
-    public void acceptMatchingRequest(Long managerId, Long contractId) {
-        Nominee nominee = getNominee(managerId, contractId);
-        nominee.updateStatus(MatchingStatus.ACCEPT);
-        nomineeRepository.save(nominee);
+    public void matchManagerAndCustomer(Long managerId, Long contractId) {
+        updateMatchingStatus(managerId, contractId, MatchingStatus.ACCEPT);
+
+        Manager manager = managerRepository.findById(managerId)
+                .orElseThrow(() -> new ManagerNotFoundException(ErrorCode.MANAGER_NOT_FOUND));
+        Contract contract = contractRepository.findById(contractId)
+                .orElseThrow(() -> new ContractException(ErrorCode.CONTRACT_NOT_FOUND));
+
+        if (!contract.isPersonal()) throw new ContractException(ErrorCode.CONTRACT_BAD_REQUEST);
+
+        contract.updateManager(manager);
+        contractRepository.save(contract);
     }
 
     private Nominee getNominee(Long managerId, Long contractId) {
