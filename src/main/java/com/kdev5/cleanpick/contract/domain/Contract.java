3d8b17d8142257f0cd0129529e0cfaf51bbbfe86
banxzxx <jwbang1643@gmail.com>
2025-05-30 17:44:54 +0900

    refactor : '}' 누락 수정

--- src/main/java/com/kdev5/cleanpick/contract/domain/Contract.java
+++ src/main/java/com/kdev5/cleanpick/contract/domain/Contract.java
@@ -75,6 +75,7 @@ public class Contract extends BaseTimeEntity {
 
     public void updateDate(LocalDateTime date) {
         this.contractDate = date;
+    }
       
     public void updateManager(Manager manager) {
         this.manager = manager;
