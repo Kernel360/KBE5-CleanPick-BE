5bb3a529dd2e4037c17979d968c2c499c522d14d
mummhy0811 <mummhy0811@gmail.com>
2025-05-30 10:23:17 +0900

    refactor: (코드리뷰) nominee컨트롤러 contract컨트롤러 병합

--- src/main/java/com/kdev5/cleanpick/contract/controller/ContractController.java
+++ src/main/java/com/kdev5/cleanpick/contract/controller/ContractController.java
@@ -1,18 +1,19 @@
 package com.kdev5.cleanpick.contract.controller;
 
+import com.kdev5.cleanpick.cleaning.domain.enumeration.ServiceName;
 import com.kdev5.cleanpick.contract.service.ContractService;
 import com.kdev5.cleanpick.contract.service.ReadContractService;
+import com.kdev5.cleanpick.contract.service.ReadNomineeService;
 import com.kdev5.cleanpick.contract.service.dto.request.ContractFilterStatus;
 import com.kdev5.cleanpick.contract.service.dto.request.ContractRequestDto;
-import com.kdev5.cleanpick.contract.service.dto.response.OneContractResponseDto;
-import com.kdev5.cleanpick.contract.service.dto.response.ReadContractDetailResponseDto;
-import com.kdev5.cleanpick.contract.service.dto.response.ReadContractResponseDto;
-import com.kdev5.cleanpick.contract.service.dto.response.RoutineContractResponseDto;
+import com.kdev5.cleanpick.contract.service.dto.response.*;
 import com.kdev5.cleanpick.global.response.ApiResponse;
 import com.kdev5.cleanpick.global.response.PageResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/contract")
 @Tag(name = "contract API", description = "계약글 CRUD 컨트롤러")
 public class ContractController {
+
+    private static final Long managerId = 1L; // TODO
+
+    private final ReadNomineeService readNomineeService;
     private final ReadContractService readContractService;
     private final ContractService contractService;
 
@@ -53,6 +58,37 @@ public class ContractController {
     @PutMapping
     public ApiResponse<OneContractResponseDto> changeContract(@RequestBody @Valid ContractRequestDto contractDto) {
         return null;
+    }
+
+    @GetMapping("/pending")
+    @Operation(summary = "매니저에게 들어온 요청 목록 조회", description = "매니저에게 들어온 모든 요청을 조회합니다.")
+    public ResponseEntity<ApiResponse<PageResponse<ReadRequestedMatchingResponseDto>>> readRequestedMatching(
+            @RequestParam(value = "isPersonal", required = false) Boolean isPersonal,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        return ResponseEntity.ok(ApiResponse.ok(new PageResponse<>(readNomineeService.readRequestedMatching(managerId, isPersonal, PageRequest.of(page, size)))));
+    }
+
+    @GetMapping("/accepted")
+    @Operation(summary = "매니저가 신청한 요청 목록 조회", description = "매니저가 수락한 모든 목록을 조회합니다.")
+    public ResponseEntity<ApiResponse<PageResponse<ReadAcceptedMatchingResponseDto>>> readAcceptedMatching(
+            @RequestParam(value = "type", required = false) ServiceName type,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        return ResponseEntity.ok(ApiResponse.ok(new PageResponse<>(readNomineeService.readAcceptedMatching(managerId, type, PageRequest.of(page, size)))));
+    }
+
+    @GetMapping("/confirmed")
+    @Operation(summary = "최종 매칭된 목록 조회", description = "매니저와 수요자 서로 accept한 목록(최종 매칭된 목록)을 조회합니다.sortType은 최근 매칭순(MATCHED), 계약일순(CONTRACT) 입니다.")
+    public ResponseEntity<ApiResponse<PageResponse<ReadConfirmedMatchingResponseDto>>> readConfirmedMatching(
+            @RequestParam(value = "serviceType", required = false) ServiceName serviceType,
+            @RequestParam(value = "sortType") String sortType,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        return ResponseEntity.ok(ApiResponse.ok(new PageResponse<>(readNomineeService.readConfirmedMatching(managerId, serviceType, sortType, PageRequest.of(page, size)))));
     }
 }
 
