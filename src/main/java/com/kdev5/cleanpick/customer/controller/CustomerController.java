ee5293e6ee4ccc26e08bc60d4a5069d146d2949b
Yeonggeun Lee <yeonggeun.lee@slexn.com>
2025-06-03 21:20:15 +0900

    feat: 청소 옵션 조회 API

--- src/main/java/com/kdev5/cleanpick/customer/controller/CustomerController.java
+++ src/main/java/com/kdev5/cleanpick/customer/controller/CustomerController.java
@@ -16,17 +16,21 @@ import com.kdev5.cleanpick.global.response.ApiResponse;
 import com.kdev5.cleanpick.global.security.annotation.CustomerId;
 import com.kdev5.cleanpick.global.security.auth.CustomUserDetails;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/customers")
+@Tag(name = "customer API", description = "고객에 대한 정보를 조회 및 상세 정보 입력용 컨트롤러")
 public class CustomerController {
 
 	private final CustomerService customerService;
 
 	@PostMapping
+	@Operation(summary = "최초 고객 정보 입력", description = "최초 고객정보가 입력되는 API 이며, 이때 최초로 데이터베이스에 등록됩니다.")
 	public ResponseEntity<ApiResponse<CustomerPrivateResponseDto>> writeCustomer(@AuthenticationPrincipal CustomUserDetails customUserDetails, @Valid @RequestBody WriteCustomerRequestDto writeCustomerRequestDto) {
 		return ResponseEntity.ok(
 			ApiResponse.ok(customerService.writeCustomer(customUserDetails.getId(), writeCustomerRequestDto))
@@ -34,6 +38,7 @@ public class CustomerController {
 	}
 
 	@PutMapping
+	@Operation(summary = "고객 정보 변경", description = "최초 등록 이후 프로필 수정을 위한 API 입니다.")
 	public ResponseEntity<ApiResponse<CustomerPrivateResponseDto>> editCustomer(@CustomerId Long customerId, @Valid @RequestBody WriteCustomerRequestDto writeCustomerRequestDto) {
 		return ResponseEntity.ok(
 			ApiResponse.ok(customerService.editCustomer(customerId, writeCustomerRequestDto))
@@ -41,6 +46,7 @@ public class CustomerController {
 	}
 
 	@GetMapping
+	@Operation(summary = "고객 정보 조회", description = "개인 정보 조회를 위한 API 입니다. 개인정보가 있어 인증이 필요합니다.")
 	public ResponseEntity<ApiResponse<CustomerPrivateResponseDto>> getCustomer(@CustomerId Long id) {
 		return ResponseEntity.ok(
 			ApiResponse.ok(customerService.getCustomer(id))
--- src/main/java/com/kdev5/cleanpick/user/controller/UserController.java
+++ src/main/java/com/kdev5/cleanpick/user/controller/UserController.java
@@ -5,6 +5,9 @@ import com.kdev5.cleanpick.global.security.auth.CustomUserDetails;
 import com.kdev5.cleanpick.user.service.UserService;
 import com.kdev5.cleanpick.user.service.dto.request.SignUpRequestDto;
 import com.kdev5.cleanpick.user.service.dto.response.UserResponseDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,11 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "user API", description = "계정 정보를 관리하는 컨트롤러")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/signup/customer")
+    @Operation(summary = "customer 회원가입", description = "타입이 customer 이며 PENDING 상태의 유저가 생성됩니다.")
     private ResponseEntity<ApiResponse<UserResponseDto>> registerCustomer(@Valid @RequestBody SignUpRequestDto customerSignUpRequestDto) {
         UserResponseDto response = userService.customerSignUp(customerSignUpRequestDto);
         return ResponseEntity
@@ -30,6 +35,7 @@ public class UserController {
     }
 
     @PostMapping("/signup/manager")
+    @Operation(summary = "manager 회원가입", description = "타입이 manager 이며 PENDING 상태인 유저가 생성됩니다.")
     private ResponseEntity<ApiResponse<UserResponseDto>> registerManager(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
         UserResponseDto response = userService.managerSignup(signUpRequestDto);
         return ResponseEntity
